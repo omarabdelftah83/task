5fa87ff676bd1fceb38779c119b035542e697d20
omar <om21801@gmail.com>
2024-02-23 18:55:21 +0200

    fix bugs

--- lib/domain/usecase/get_list_useCase.dart
+++ lib/domain/usecase/get_list_useCase.dart
@@ -3,7 +3,6 @@ import 'package:task/domain/repositry/base_list_repo.dart';
 
 import '../../core/error/faluire.dart';
 import '../entites/list.dart';
-
 class GetListUseCase{
   BaseListRepository baseListRepository;
   GetListUseCase(this.baseListRepository);
